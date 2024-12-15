dcaa0773a28fc557bcaa3b899f3c1a34367358f8
xuleyman <74072560+xuleyman@users.noreply.github.com>
2024-12-15 20:38:28 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
   fillWidth
   minHeight="0"
   maxWidth={68}
-  direction="row"  // Yan yana hizalamak için
+  direction="row"  // Logo ve yazıyı yan yana hizalamak için
   alignItems="center"  // Dikeyde ortalamak için
   justifyContent="center"  // Yatayda ortalamak için
   flex={1}
@@ -125,39 +125,8 @@ export default function Home() {
       </Heading>
     </Flex>
   </Flex>
-
-  {/* Diğer içerik (linkler vs.) */}
-  <Grid
-    radius="l"
-    border="neutral-medium"
-    borderStyle="solid-1"
-    columns="repeat(3, 1fr)"
-    tabletColumns="1col"
-    mobileColumns="1col"
-    fillWidth
-  >
-    {links.map((link) => (
-      <Link
-        target="_blank"
-        style={{ padding: 'var(--responsive-space-l)' }}
-        key={link.href}
-        href={link.href}
-      >
-        <Flex fillWidth paddingY="8" gap="8" direction="column">
-          <Flex fillWidth gap="12" alignItems="center">
-            <Text variant="body-strong-m" onBackground="neutral-strong">
-              {link.title}
-            </Text>
-            <Icon size="s" name="arrowUpRight" />
-          </Flex>
-          <Text variant="body-default-s" onBackground="neutral-weak">
-            {link.description}
-          </Text>
-        </Flex>
-      </Link>
-    ))}
-  </Grid>
 </Flex>
+
 
 			<Flex
 				as="footer"
