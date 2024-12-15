325c093d32d376b01a33cdc22e59a90cb11f4cfa
xuleyman <74072560+xuleyman@users.noreply.github.com>
2024-12-15 19:42:32 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
 							<img
   src="/coconode.png"  // public klasöründeki logo yolu
   alt="My Logo"
-  style={{ width: '1000px', height: 'auto' }}  // Boyutlandırma yapabilirsiniz
+  style={{ width: '200px', height: 'auto' }}  // Boyutlandırma yapabilirsiniz
 />
 
 						</Flex>
