604bdcd2a28c2710b6ba3acce3b5edc2f9ad1a75
xuleyman <74072560+xuleyman@users.noreply.github.com>
2025-02-11 19:24:30 +0300

    Update config.js

--- src/once-ui/resources/config.js
+++ src/once-ui/resources/config.js
@@ -5,11 +5,11 @@ const style = {
     theme:       'light',        // dark | light
     neutral:     'gray',        // sand | gray | slate
     brand:       'cyan',        // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    accent:      'indigo',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    accent:      'red',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     solid:       'color',    // color | contrast
     solidStyle:  'flat',        // flat | plastic
     border:      'playful',     // rounded | playful | conservative
-    surface:     'translucent', // filled | translucent
+    surface:     'filled', // filled | translucent
     transition:  'all',         // all | micro | macro
     scaling:     '100',         // 90 | 95 | 100 | 105 | 110
 }
